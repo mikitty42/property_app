c22a6668eed3252884f8b505d631956ff6b3dd7b
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2023-06-24 17:04:35 +0900

    propertyコントローラーを作成しました

---
+++ db/migrate/20230624064550_create_properties.rb
@@ -0,0 +1,14 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :property_name
+      t.integer :rent
+      t.string :address
+      t.string :text
+      t.integer :age
+      t.text :remarks
+
+      t.timestamps
+    end
+  end
+end
